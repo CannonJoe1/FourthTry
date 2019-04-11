d3c86ba52cc2c12ec85df0ab40f86c842a3bfb94
user <user@JoeLap>
2019-04-10 20:09:03 -0600

    Please work

--- EZDemo/src/HelloWorld.java
+++ EZDemo/src/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
 		System.out.println("Hello World!");
 		System.out.println("I am pretty fun");
 		System.out.println("No, VERY fun");
+		System.out.println("Fun is fun");
 
 		
 		for (int i=0; i < 5; i++) {
